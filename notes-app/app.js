47e0210188fa1f1cdc684e8f2bae0b6a4ab2c156
sevans <sevans@highdesertmilk.com>
2023-10-03 15:41:03 -0600

    Video 17. Argument Parsing with Yargs: Part 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,12 +5,29 @@ const getNotes = require('./notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
+// in the command line input "node app.js stuff". This would add the inputed "stuff" after the 2 default location args.
+// after inputing "node app.js" you can add a space and then input more arguments such as "add"
+
 // Create add command, you can see what options there are using "filename.js --help"
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () { // This is the code that runs when someone selects remove
-        console.log('Adding a new note!')
+    builder: { // Defines all the options you want this given command to support.
+        title: { // ex. node app.js add --title='My Title'
+            describe: 'Note title', 
+            demandOption: true,  // Would require that the title option is inputed if the "add" command is selected.
+            type: 'string' // requires that a string is inputed, or will create an empty string if one isn't.
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) { // This is the code that runs when someone selects the command
+        console.log(`Title: ${argv.title}`)
+        console.log(`Body: ${argv.body}`)
+
     }
 })
 
@@ -41,6 +58,7 @@ yargs.command({
     }
 })
 
-// in the command line input "node app.js stuff". This would add the inputed "stuff" after the 2 default location args.
+
         // Was process.argv, argv is an array of all the arguments provided.
-console.log(yargs.argv) // after inputing "node app.js" you can add a space and then input more arguments such as "add"
+//console.log(yargs.argv) // This line of code is needed in order for yargs to know when and how to parse its arguments.
+yargs.parse() // An alternative way to the one above, this one isn't as optimal, it doesn't take any arguments.

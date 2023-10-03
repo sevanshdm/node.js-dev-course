cb82ee6e97550c9c033141a13e22df0ecfc44983
sevans <sevans@highdesertmilk.com>
2023-10-03 15:02:04 -0600

    Video 16. Argument Parsing with Yargs: Part 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-                        // this arrays index would target the "add" input provided after the 2 default location args load.
-const command = process.argv[2] // in the command line input "node app.js add". argv is an array of all the arguments provided.
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv) // after inputing "node app.js" you can add a space and then input more arguments such as "add"
+// Create add command, you can see what options there are using "filename.js --help"
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () { // This is the code that runs when someone selects remove
+        console.log('Adding a new note!')
+    }
+})
 
-if (command === 'add') { 
-    console.log('Adding note!')
-} else if (command ==='remove') {
-    console.log('Removing note!')
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () { 
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+// in the command line input "node app.js stuff". This would add the inputed "stuff" after the 2 default location args.
+        // Was process.argv, argv is an array of all the arguments provided.
+console.log(yargs.argv) // after inputing "node app.js" you can add a space and then input more arguments such as "add"

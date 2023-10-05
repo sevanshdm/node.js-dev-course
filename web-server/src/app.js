ca92fc6f778372932f77860ebbe72a36871720fe
sevans <sevans@highdesertmilk.com>
2023-10-05 15:19:45 -0600

    Video 44. Server up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,24 +5,31 @@ const app = express()
 // app.com
       //route, function of what to do 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 // app.com/help
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Shrek'
+    }, {
+        name: 'Donkey'
+    }])
 })
 
 // app.com/about
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About Page</h1>')
 })
 
 // app.com/weather
 app.get('/weather', (req, res) => {
-    res.send('View weather.')
+    res.send({
+        forecast: 'Nasty',
+        location: 'Thee Swamp'
+    })
 })
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+    console.log('Server is up on port 3000')
 })

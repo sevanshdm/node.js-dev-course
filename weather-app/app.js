706025d477182f0f3d2c11575a254f5088159cf1
sevans <sevans@highdesertmilk.com>
2023-10-05 12:19:15 -0600

    Video 38. Callback Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,27 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-                //good practice to have an error and response option
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
-forecast(37.775, -122.418, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+if (!address) {
+    console.log('Please provide an adress')
+} else {
+
+                    //good practice to have an error and response option
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        } 
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location,)
+            console.log(forecastData)
+        })
+    })
+}
+
+

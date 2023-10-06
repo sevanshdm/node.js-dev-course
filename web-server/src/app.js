1671be14f6d86683cd4236a306b7b67a8015050f
sevans <sevans@highdesertmilk.com>
2023-10-06 08:29:42 -0600

    Video 45. Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-// app.com
-      //route, function of what to do 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-// app.com/help
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Shrek'
-    }, {
-        name: 'Donkey'
-    }])
-})
 
-// app.com/about
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>')
-})
 
 // app.com/weather
 app.get('/weather', (req, res) => {

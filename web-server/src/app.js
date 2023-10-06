f176febb57b53f9ea57f37a0993cf8e188f7d54a
sevans <sevans@highdesertmilk.com>
2023-10-06 09:49:03 -0600

    Video 47. Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,31 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+app.set('views', path.join(__dirname, '../views')) //That'll make sure it always uses the correct views folder no matter where you run the project from.
+app.set('view engine', 'hbs') // hbs is handlebars
+// index template
+app.get('', (req, res) => {
+            //index.hbs from the views folder
+    res.render('index',{
+        title: 'Weather',
+        name: 'Shrek Swampington'
+    }) // I've configured to express to use view engine "hbs", so render will be able to render .hbs templates.
+})
 
+// app.com/about template
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Shrek Swampington'
+    })
+})
+
+// app.com/help
+app.get('/help', (req,res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
 
 // app.com/weather
 app.get('/weather', (req, res) => {

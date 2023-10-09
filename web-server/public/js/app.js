1eef2fe3fe5be3ed20e3eae2d68cbe410da5564c
sevans <sevans@highdesertmilk.com>
2023-10-09 09:56:05 -0600

    Video 59. Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,26 +1,25 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data)=> { //returns parsed data
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From JavaSript'
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
-
     const location = search.value
+    messageOne.textContent ='Loading...'
+    messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })

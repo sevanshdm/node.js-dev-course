0b7c6e282d54239dae569a85f74732a351947e63
sevans <sevans@highdesertmilk.com>
2023-10-09 09:00:35 -0600

    Video 58. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,28 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data)=> { //returns parsed data
         console.log(data)
     })
 })
 
-fetch('http://localhost:3000/weather?address=franca').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
+
 })

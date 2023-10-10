96478c759d37c1c59a2582e5e09aae9d1ebb8b8b
sevans <sevans@highdesertmilk.com>
2023-10-10 13:51:51 -0600

    Video 77. The ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient // An object, gives access to function necessary to connect to database so you can perform CRUD ops.
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient // An object, gives access to function necessary to connect to database so you can perform CRUD ops.
+// const ObjectID = mongodb.ObjectId
+
+// destructured version of above code
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectId() // Generate your own id's from the file
+console.log(id)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -13,8 +20,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({ //inserts a simple document into the users collection
-        name: 'Shrek',
-        age: '33'
-    })
+    // db.collection('users').insertOne({ //inserts a simple document into the users collection
+    //     name: 'Henry',
+    //     age: '67'
+    // }, (error, results) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user.')
+    //     }
+
+    //     console.log(results.insertedId)
+    // })
+    
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Donkey',
+    //         age: '36'
+    //     }, {
+    //         name: 'Gingy',
+    //         age: '127'
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert userts.')
+    //     }
+
+    //     console.log(result.insertedIds)
+    // })
+
+    // db.collection('tasks').insertMany([{
+    //     description: 'Clean the house',
+    //     completed: true
+    // },{
+    //     description: 'Renew inspection',
+    //     completed: false
+    // },{
+    //     description: 'Pot plants',
+    //     completed: false
+    // }], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert tasks.')
+    //     }
+
+    //     console.log(result.insertedIds)
+    // })
 })

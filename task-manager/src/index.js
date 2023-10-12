385dda69d9b9e0d83e4d41f3f32ef46b32bf6d1b
sevans <sevans@highdesertmilk.com>
2023-10-12 09:59:54 -0600

    Video 92

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -41,7 +41,7 @@ app.get('/users/:id', (req,res) => {
         }
 
         res.send(user)
-        
+
     }).catch((e) => {
         res.status(500).send()
     })
@@ -54,6 +54,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks',(req, res) => {
+    Task.find({}).then((tasks)=> {
+        res.send(tasks)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id
+
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send()
     })
 })
 

64197ceda2f297b750b7554089bfcae8e92402da
sevans <sevans@highdesertmilk.com>
2023-10-12 13:29:24 -0600

    Video 97. Integrating Async/Await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,72 +10,112 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // request handlers
-app.post('/users', (req, res)=> {
+app.post('/users', async (req, res)=> {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        //saves the user
+        await user.save()
+        // code below will run if the above line works.
         res.status(201).send(user) //sends back the status (created) and result.
-    }).catch((e) => {
+    }catch (e) {
         res.status(400).send(e) //sends back the status to the client so they know what kind of error it is.
-        
-    })
+    }
 })
 
 // route handler for fetching multiple users
-app.get('/users', (req,res) => {
-    User.find({}).then((users) => { // leaving the object as empty fetches all users stored in the database
+app.get('/users', async (req,res) => {
+
+    try {
+        const users = await User.find({}) // leaving the object as empty fetches all users stored in the database
         res.send(users)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send() // sends internal service error message
-    })
+    }
 })
 
 // route handler to fetch an individual user by ID
      // (dynamic) the ID of the user trying to fetch with express's "route parameters" which are parts of the url that are used to capture dynamic values.
-app.get('/users/:id', (req,res) => {
+app.get('/users/:id', async (req,res) => {
     const _id = req.params.id //matches with up above, this contains all the route parameters that were provided.
 
-    User.findById(_id).then((user) => {
-        if(!user) {
+    try{
+        const user = await User.findById(_id)
+
+        if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
+    }catch (e) {
+        res.status(500)
+    }
 
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    // User.findById(_id).then((user) => {
+    //     if(!user) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(user)
+
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async(req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    }catch(e) {
         res.status(400).send(e)
-    })
+    }
+    // task.save().then(() => {
+    //     res.status(201).send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
-app.get('/tasks',(req, res) => {
-    Task.find({}).then((tasks)=> {
+app.get('/tasks', async(req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e)=>{
+    }catch(e){
         res.status(500).send()
-    })
+
+    }
+    // Task.find({}).then((tasks)=> {
+    //     res.send(tasks)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id',(req,res)=>{
+app.get('/tasks/:id', async (req,res)=>{
     const _id = req.params.id
 
-    Task.findById(_id).then((task)=>{
+    try {
+        const task = await Task.findById(_id)
+
         if(!task){
-            return res.status(404).send()
+            return res.status(404).send
         }
         res.send(task)
-    }).catch((e)=>{
+    }catch(e){
         res.status(500).send()
-    })
+    }
+
+    // Task.findById(_id).then((task)=>{
+    //     if(!task){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
 })
 
 app.listen(port, () => {

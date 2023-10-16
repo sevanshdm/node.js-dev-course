f45247b00eb40386cc35655778ef31373fb31e1f
sevans <sevans@highdesertmilk.com>
2023-10-16 09:26:34 -0600

    Video 113. Authenticating User Endpoints

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,7 +3,6 @@ const User = require('../models/user')
 // Use methods on router to customize it (router.post, router.get, router.patch, router.delete)
 const router = new express.Router()
 const auth = require('../middleware/auth')
-
 // request handlers
 // Sign up a new user
 router.post('/users', async (req, res)=> {
@@ -58,43 +57,17 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 })
 
 // route handler for fetching multiple users
+// (dynamic) the ID of the user trying to fetch with express's "route parameters" which are parts of the url that are used to capture dynamic values.
 router.get('/users/me', auth, async (req,res) => { // when /users receives a GET request, it first runs the middleware(auth), then it runs route handler.
     res.send(req.user)
 //const users = await User.find({}) // leaving the object as empty fetches all users stored in the database
 })
 
-// route handler to fetch an individual user by ID
-     // (dynamic) the ID of the user trying to fetch with express's "route parameters" which are parts of the url that are used to capture dynamic values.
-router.get('/users/:id', async (req,res) => {
-    const _id = req.params.id //matches with up above, this contains all the route parameters that were provided.
-
-    try{
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }catch (e) {
-        res.status(500)
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if(!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-// express route handler for updating user by id
-router.patch('/users/:id', async (req, res) =>{
+// express route handler for updating user
+router.patch('/users/me', auth, async (req, res) =>{
     const updates = Object.keys(req.body) //converts an object into an array of its properties
+
+
     const allowedUpdates = ['name', 'email', 'password', 'age']
 
     const isValidOperation = updates.every((update)=> allowedUpdates.includes(update)) // function runs for every item in the array
@@ -104,28 +77,25 @@ router.patch('/users/:id', async (req, res) =>{
     }
 
     try { // Where to apply change, The data for the id will passed through via the HTTP request.
-        const user = await User.findById(req.params.id)
 
-        updates.forEach((update)=> user[update] = req.body[update])
-        await user.save()
-
-        if(!user){
-            return res.status(404).send()
-        }
+        updates.forEach((update)=> req.user[update] = req.body[update])
+        await req.user.save()
         
-        res.send(user)
+        res.send(req.user)
     }catch(e) {                                                                      
         res.status(400).send(e)
     } 
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        // const user = await User.findByIdAndDelete(req.user._id)
+        // if(!user){
+        //     return res.status(404).send()
+        // }
 
-        if(!user){
-            return res.status(404).send()
-        }
+        await req.user.remove()
+        
         res.send(user)
     }catch(e){
         res.status(500).send()

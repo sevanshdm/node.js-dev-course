d399dcb24d794789359b8733a3814466384a0618
sevans <sevans@highdesertmilk.com>
2023-10-17 13:20:27 -0600

    Video 132.

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -4,9 +4,27 @@ const sendgridAPIKey = 'Insert Key Here'
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to:'',
-    from: '',
-    subject: '',
-    text: ''
-})
+const sendWelcomeEmail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: 'sevans@highdesertmilk.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+        //html: '' if you're feeling fancy
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'sevans@highdesertmilk.com',
+        subject: 'Sorry to see you go',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}

983da0cc48374ff5fa5ff90c7f908081cef8c42e
sevans <sevans@highdesertmilk.com>
2023-10-17 08:57:30 -0600

    Video 124. Validating File Uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,34 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+
+const multer = require('multer')
+const upload = multer({ //options object
+    dest: 'images',
+    limits: {
+        fileSize: 1000000, // 1 mb
+    },
+    //             object  callback
+    fileFilter(req, file, cb) { //function that runs when a new file is attempted to be uploaded.
+        if(!file.originalname.match(/\.(doc|docx)$/)){ // this is a regular expression
+
+              //(!file.originalname.endsWith('.pdf')) { // only accepts media files
+            return cb(new Error('Please upload a Word document.'))      
+        }   
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a PDF')) // error
+        // cb(undefined, true) // if things go right
+        // cb(undefined, false) // silently rejects file
+    }
+})
+                // Multer middleware
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+}) 
+
+
 // automatically parses incoming json into an object.
 app.use(express.json())
 

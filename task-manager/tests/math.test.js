ab993dec014906bf8227e63a231e8440c1c21692
sevans <sevans@highdesertmilk.com>
2023-10-18 10:18:19 -0600

    Video 140. Testing Asynchronous Code

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,5 +1,5 @@
    // Destructure the object to grab the individual functions as variables.
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math') // exports from this file are in an object.
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math') // exports from this file are in an object.
 
 test('Should calculate total with tip', ()=>{
     const total = calculateTip(10, .3)
@@ -23,6 +23,34 @@ test('Should convert 0 C to 32 F', ()=>{
     const temp = celsiusToFahrenheit(0)
 
     expect(temp).toBe(32)
+})
+
+                    // This tells jest that this function contains async code. 
+// test('Async test demo', (done)=>{ // Done is called after everything is complete.
+//     setTimeout(()=>{
+//         try{
+//             expect(1).toBe(2)
+//             done()
+//         }catch(e){
+//             done(e)
+//         }
+//     },2000)
+// })
+
+test('Should add two numbers', (done)=>{
+    add(2,3).then((sum)=>{
+        try{
+            expect(sum).toBe(5)
+            done()
+        }catch(e){
+            done(e)
+        }
+    })
+})
+
+test('Should add two number async/await', async()=>{
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
 
 // Why test?

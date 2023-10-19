48cdfa515f9f17896456510987abe2a65c9d1dcd
sevans <sevans@highdesertmilk.com>
2023-10-19 09:21:04 -0600

    Video 149. Testing with Task Data

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 // Jest setup and teardown
 // Jest lifecycle methods, runs some code just before or after a test case. That will allow you set up some code that clears the database so that your test cases consistently execute as expected.
@@ -20,14 +21,63 @@ const userOne = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+
+const userTwo = {
+    _id: userTwoId,
+    name: 'Bongo',
+    email: 'bigbacon@example.com',
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET) // creates a new json web token
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First task',
+    completed: false,
+    owner: userOne._id
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second task',
+    completed: true,
+    owner: userOne._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third task',
+    completed: true,
+    owner: userTwo._id
+}
+
 const setupDatabase = async () =>{
     await User.deleteMany()
+    await Task.deleteMany()
 
     await new User(userOne).save() // creates a user to test with after all the previous ones were deleted.
+    await new User(userTwo).save()
+
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
     userOneId,
+    userTwoId,
     userOne,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
     setupDatabase
 }
+
+// This prevents the "A worker process has failed to exit gracefully and has been force exited" error
+afterAll(() => {
+    mongoose.connection.close();
+})
--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,17 @@
 const request = require('supertest')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const {
+    userOneId, 
+    userTwoId,
+    userOne, 
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase,
+} = require('./fixtures/db')
 
 // runs before each test case in this test suite and deletes all the users in the testing Database so that you don't run into any errors with duplicates.
 beforeEach(setupDatabase)
@@ -18,3 +28,27 @@ test('Should create task for user', async ()=>{
     expect(task).not.toBeNull()
     expect(task.completed).toBe(false)
 }) 
+
+test('Should fetch user tasks', async ()=>{
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete other users tasks', async ()=>{
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
+
+// This prevents the "A worker process has failed to exit gracefully and has been force exited" error
+afterAll(() => {
+    mongoose.connection.close();
+})
